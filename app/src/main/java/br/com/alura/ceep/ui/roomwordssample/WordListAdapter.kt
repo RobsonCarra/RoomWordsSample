f12d1a8a93bf50b36f07dccc5e0304b1de7658f2
Robson Simionato Carra <robcarra18@hotmail.com>
2021-08-20 12:27:41 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/ceep/ui/roomwordssample/WordListAdapter.kt
@@ -0,0 +1,52 @@
+package br.com.alura.ceep.ui.roomwordssample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+class WordListAdapter : RecyclerView.Adapter<WordViewHolder>() {
+
+    var list = ArrayList<Word>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.word)
+    }
+
+    fun getItem(position: Int): Word {
+        return list.get(position)
+    }
+
+    fun getItemFirst(): Word {
+        return list.first()
+    }
+
+    fun getItemLast(): Word {
+        return list.last()
+    }
+
+
+    override fun getItemCount(): Int {
+        return list.count()
+    }
+
+
+    companion object {
+        private val WORDS_COMPARATOR = object : DiffUtil.ItemCallback<Word>() {
+            override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+                return oldItem.word == newItem.word
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/alura/ceep/ui/roomwordssample/WordsApplication.kt
@@ -0,0 +1,10 @@
+package br.com.alura.ceep.ui.roomwordssample
+
+import android.app.Application
+
+class WordsApplication : Application() {
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
