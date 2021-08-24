e0d73456eb44a9f78c98e176c127149bde3f2b84
Pierry Borges <pieerry@gmail.com>
2021-08-24 18:17:59 -0300

    Melhorias

--- app/src/main/java/br/com/alura/ceep/ui/roomwordssample/UserRepository.kt
+++ app/src/main/java/br/com/alura/ceep/ui/roomwordssample/UserRepository.kt
@@ -2,11 +2,14 @@ package br.com.alura.ceep.ui.roomwordssample
 
 class UserRepository(private val userDao: UserDao) {
 
-    fun getByUser(adress: String, key: String) = userDao.getUser(adress, key)
+  fun getByUser(adress: String, key: String) = userDao.getUser(adress, key)
 
-    fun add(user: User): Boolean {
-        userDao.putUser(user)
-        return true
+  fun add(user: User): Boolean {
+    val id = userDao.putUser(user)
+    if (id > 0) {
+      return true
+    } else {
+      return false
     }
-
+  }
 }
--- app/src/main/java/br/com/alura/ceep/ui/roomwordssample/WordViewModel.kt
+++ app/src/main/java/br/com/alura/ceep/ui/roomwordssample/WordViewModel.kt
@@ -8,85 +8,86 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WordViewModel(
-    private val wordRepository: WordRepository,
-    private val userRepository: UserRepository
+  private val wordRepository: WordRepository,
+  private val userRepository: UserRepository
 
 ) : ViewModel() {
-    val wordList = MutableLiveData<List<Word>>()
-    val filteredByName = MutableLiveData<List<Word>>()
-    val filteredBySize = MutableLiveData<List<Word>>()
-    val filteredByDecription = MutableLiveData<List<Word>>()
-    val filteredByPrice = MutableLiveData<List<Word>>()
-    val filteredByUser = MutableLiveData<List<User>>()
+  val wordList = MutableLiveData<List<Word>>()
+  val filteredByName = MutableLiveData<List<Word>>()
+  val filteredBySize = MutableLiveData<List<Word>>()
+  val filteredByDecription = MutableLiveData<List<Word>>()
+  val filteredByPrice = MutableLiveData<List<Word>>()
+  val filteredByUser = MutableLiveData<List<User>>()
+  val added = MutableLiveData<Boolean>()
 
-
-    fun getAll() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val words = wordRepository.get()
-            wordList.postValue(words)
-        }
+  fun getAll() {
+    viewModelScope.launch(Dispatchers.IO) {
+      val words = wordRepository.get()
+      wordList.postValue(words)
     }
+  }
 
-    fun getByName(name: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val words = wordRepository.getByName(name)
-            filteredByName.postValue(words)
-        }
+  fun getByName(name: String) {
+    viewModelScope.launch(Dispatchers.IO) {
+      val words = wordRepository.getByName(name)
+      filteredByName.postValue(words)
     }
+  }
 
-    fun getBySize(number: Int) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val numbers = wordRepository.getBySize(number)
-            filteredBySize.postValue(numbers)
-        }
+  fun getBySize(number: Int) {
+    viewModelScope.launch(Dispatchers.IO) {
+      val numbers = wordRepository.getBySize(number)
+      filteredBySize.postValue(numbers)
     }
+  }
 
-    fun getByDescription(information: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val informations = wordRepository.getByDescription(information)
-            filteredByDecription.postValue(informations)
-        }
-
+  fun getByDescription(information: String) {
+    viewModelScope.launch(Dispatchers.IO) {
+      val informations = wordRepository.getByDescription(information)
+      filteredByDecription.postValue(informations)
     }
+  }
 
-    fun getByPrice(cost: Double) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val total = wordRepository.getByPrice(cost)
-            filteredByPrice.postValue(total)
-        }
-
+  fun getByPrice(cost: Double) {
+    viewModelScope.launch(Dispatchers.IO) {
+      val total = wordRepository.getByPrice(cost)
+      filteredByPrice.postValue(total)
     }
+  }
 
-    fun add(word: Word) {
-        viewModelScope.launch(Dispatchers.IO) {
-            wordRepository.add(word)
-        }
+  fun add(word: Word) {
+    viewModelScope.launch(Dispatchers.IO) {
+      wordRepository.add(word)
     }
+  }
 
-    fun getByUser(adress: String, key: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val Users = userRepository.getByUser(adress, key)
-            filteredByUser.postValue(Users)
-        }
+  fun getByUser(adress: String, key: String) {
+    viewModelScope.launch(Dispatchers.IO) {
+      val users = userRepository.getByUser(adress, key)
+      filteredByUser.postValue(users)
     }
+  }
 
-    fun addUser(user: User) {
-        viewModelScope.launch(Dispatchers.IO) {
-            userRepository.add(user)
-        }
+  fun addUser(user: User) {
+    viewModelScope.launch(Dispatchers.IO) {
+      val saved = userRepository.add(user)
+      if (saved){
+        added.postValue(true)
+      }
     }
+  }
 
-    class WordViewModelFactory(
-        private val wordRepository: WordRepository,
-        private val userRepository: UserRepository
-    ) :
-        ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST")
-                return WordViewModel(wordRepository, userRepository) as T
-            }
-            throw IllegalArgumentException("Unknown ViewModel class")
-        }
+  class WordViewModelFactory(
+    private val wordRepository: WordRepository,
+    private val userRepository: UserRepository
+  ) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+        @Suppress("UNCHECKED_CAST")
+        return WordViewModel(wordRepository, userRepository) as T
+      }
+      throw IllegalArgumentException("Unknown ViewModel class")
     }
+  }
 }
