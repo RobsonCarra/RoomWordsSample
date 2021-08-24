d21a567c83018d0122fb22dfe4ecb07c330ff58d
Pierry Borges <pieerry@gmail.com>
2021-08-24 09:59:10 -0300

    New tasks

--- app/src/main/java/br/com/alura/ceep/ui/roomwordssample/WordsApplication.kt
+++ app/src/main/java/br/com/alura/ceep/ui/roomwordssample/WordsApplication.kt
@@ -3,14 +3,13 @@ package br.com.alura.ceep.ui.roomwordssample
 import android.app.Application
 
 class WordsApplication : Application() {
-    // Using by lazy so the database and the repository are only created when they're needed
-    // rather than when the application starts
-    private val database by lazy { WordRoomDatabase.getDatabase(this) }
-    private val database2 by lazy { WordRoomDatabase.getDatabase(this) }
-    val repository by lazy {
-        WordRepository(database.wordDao())
-    }
-    val repository2 by lazy {
-        UserRepository(database2.userDao())
-    }
+  // Using by lazy so the database and the repository are only created when they're needed
+  // rather than when the application starts
+  private val database by lazy { WordRoomDatabase.getDatabase(this) }
+  val repository by lazy {
+    WordRepository(database.wordDao())
+  }
+  val repository2 by lazy {
+    UserRepository(database.userDao())
+  }
 }
